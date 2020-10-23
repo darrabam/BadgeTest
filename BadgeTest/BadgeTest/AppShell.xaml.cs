d7df74258a0a6b50132c8471cc0ba5882a9e5978
Aziz Al-Darrab <aziz_darrab@hotmail.com>
2020-10-23 15:07:40 +0300

    Add project files.

---
+++ BadgeTest/BadgeTest/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using BadgeTest.ViewModels;
+using BadgeTest.Views;
+using Xamarin.Forms;
+
+namespace BadgeTest
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
